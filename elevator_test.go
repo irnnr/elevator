6b06875913b0f6dc48daf1b5b8c7ac5678a75823
Ingo Renner <ingo.renner@gmail.com>
2015-08-25 08:55:44 -0700

    Adjust assert methods to test for equal value

--- elevator_test.go
+++ elevator_test.go
@@ -39,12 +39,12 @@ func TestStepMovesElevatorOneFloorDownForDirectionDown(t *testing.T) {
 	el := Elevator{1, 100, 90, Down}
 	el.Step()
 
-	assert.Equal(t, 99, el.CurrentFloor)
+	assert.EqualValues(t, 99, el.CurrentFloor)
 }
 
 func TestStepMovesElevatorOneFloorUpForDirectionUp(t *testing.T) {
 	el := Elevator{1, 1, 90, Up}
 	el.Step()
 
-	assert.Equal(t, 2, el.CurrentFloor)
+	assert.EqualValues(t, 2, el.CurrentFloor)
 }
